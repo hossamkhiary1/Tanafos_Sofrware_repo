4baf044b1314039e0cc99d2a68102c3f861568ff
SaidElsoudany <62873010+SaidElsoudany@users.noreply.github.com>
2020-03-31 10:42:59 +0200

    Update MTRDRV_Cfg.h

--- Source_Code/HAL/Motor/MTRDRV_Cfg.h
+++ Source_Code/HAL/Motor/MTRDRV_Cfg.h
@@ -1,7 +1,12 @@
 #ifndef MTRDRV_Cfg_H
 #define MTRDRV_Cfg_H
-
 #define DUTY_CYCLE_PER_1_RPM  5
 #define GET_DUTY_CYCLE_FROM_RPM(RPM) (DUTY_CYCLE_PER_1_RPM * RPM) % 100
-
+#define MTR_NUM_OF_MOTORS 1
+extern const MTRConfigType BUT_ConfigParam[MTR_NUM_OF_MOTORS];
 #endif
+
+
+
+
+
