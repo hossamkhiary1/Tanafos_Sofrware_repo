cf4b603f1c0d11880848f110eb292f412dc39b06
SaidElsoudany <62873010+SaidElsoudany@users.noreply.github.com>
2020-03-31 10:42:28 +0200

    Update MTRDRV.h

--- Source_Code/HAL/Motor/MTRDRV.h
+++ Source_Code/HAL/Motor/MTRDRV.h
@@ -1,9 +1,16 @@
 #ifndef MTRDRV_H
 #define MTRDRV_H
 typedef enum {DIR_FWD, DIR_REV} MotorDirectionType;
+typedef enum {LOW, HIGH} Level;
+typedef struct 
+{
+	unsigned char DioGroupId_IN1;
+	unsigned char DioGroupId_IN2;
+	unsigned char ChannelId_EN;
+}MTRConfigType;
 void MTRDRV_Init(void);
-void MTRDRV_SetDirection(MotorDirectionType Direction);
-void MTRDRV_SetSpeed(unsigned long int RotPerMin);
-void MTRDRV_Stop(void);
+void MTRDRV_SetDirection(unsigned char Motor_Index, MotorDirectionType Direction);
+void MTRDRV_SetSpeed(unsigned char Motor_Index, unsigned long int RotPerMin);
+void MTRDRV_Stop(unsigned char Motor_Index);
 
 #endif
