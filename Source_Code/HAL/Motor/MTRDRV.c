8a471f066a406c100cf13529b6df9bf6ea3f8090
SaidElsoudany <62873010+SaidElsoudany@users.noreply.github.com>
2020-04-02 10:33:44 +0200

    Update MTRDRV.c

--- Source_Code/HAL/Motor/MTRDRV.c
+++ Source_Code/HAL/Motor/MTRDRV.c
@@ -15,25 +15,21 @@ void MTRDRV_SetDirection(unsigned char Motor_Index, MotorDirectionType Direction
 	switch(Direction)
 	{
 		case DIR_FWD:
-				DIO_Write(BUT_ConfigParam[Motor_Index].DioGroupId_IN1, HIGH);
-				DIO_Write(BUT_ConfigParam[Motor_Index].DioGroupId_IN2, LOW);
+				DIO_Write(BUT_ConfigParam[Motor_Index].DioGroupId_DIR, HIGH);
 				break;
 		case DIR_REV:
-				DIO_Write(BUT_ConfigParam[Motor_Index].DioGroupId_IN1, LOW);
-				DIO_Write(BUT_ConfigParam[Motor_Index].DioGroupId_IN2, HIGH);
-				break;
+				DIO_Write(BUT_ConfigParam[Motor_Index].DioGroupId_DIR, LOW);
+				break
 	}
 	
 	
 }
 void MTRDRV_SetSpeed(unsigned char Motor_Index, unsigned long int RotPerMin)
 {
-	PWM_SetDutyCycle(BUT_ConfigParam[Motor_Index].ChannelId_EN, GET_DUTY_CYCLE_FROM_RPM(RotPerMin));
+	PWM_SetDutyCycle(BUT_ConfigParam[Motor_Index].ChannelId_PWM, GET_DUTY_CYCLE_FROM_RPM(RotPerMin));
 }
 void MTRDRV_Stop(unsigned char Motor_Index);
 {
-	DIO_Write(BUT_ConfigParam[Motor_Index].DioGroupId_IN1, LOW);
-	DIO_Write(BUT_ConfigParam[Motor_Index].DioGroupId_IN2, LOW);
-	PWM_SetDutyCycle(BUT_ConfigParam[Motor_Index].ChannelId_EN, LOW);
+	PWM_SetDutyCycle(BUT_ConfigParam[Motor_Index].ChannelId_PWM, LOW);
 }
 
