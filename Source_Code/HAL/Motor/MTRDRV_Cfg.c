9fdb8dd1cda56fa4f60c5c03bbe52f4d0734494b
SaidElsoudany <62873010+SaidElsoudany@users.noreply.github.com>
2020-04-07 22:25:46 +0200

    Update MTRDRV_Cfg.c

--- Source_Code/HAL/Motor/MTRDRV_Cfg.c
+++ Source_Code/HAL/Motor/MTRDRV_Cfg.c
@@ -1,11 +1,12 @@
 #include "MTRDRV.h"
 #include "MTRDRV_Cfg.h"
+#include "PWM.h"
 
 const MTRConfigType MOTOR_ConfigParam[MTR_NUM_OF_MOTORS] = 
 {
 	{
 		0x00,
 		0x00,
-		0x00
+		Freq_488Hz
 	}
 };
