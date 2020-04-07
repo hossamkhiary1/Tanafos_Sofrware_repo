899d115aa7fccee5c3a5e51b758dfee16879f984
SaidElsoudany <62873010+SaidElsoudany@users.noreply.github.com>
2020-04-07 22:24:57 +0200

    Update MTRDRV_Cfg.c

--- Source_Code/HAL/Motor/MTRDRV_Cfg.c
+++ Source_Code/HAL/Motor/MTRDRV_Cfg.c
@@ -1,9 +1,10 @@
 #include "MTRDRV.h"
 #include "MTRDRV_Cfg.h"
 
-const MTRConfigType BUT_ConfigParam[MTR_NUM_OF_MOTORS] = 
+const MTRConfigType MOTOR_ConfigParam[MTR_NUM_OF_MOTORS] = 
 {
 	{
+		0x00,
 		0x00,
 		0x00
 	}
